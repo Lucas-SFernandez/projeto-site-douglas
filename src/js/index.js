4388780b0b310ee667e8b6ca0f2813e247fe87aa
Lucas-SFernandez <127568078+Lucas-SFernandez@users.noreply.github.com>
2023-10-09 13:16:48 -0300

    Commit inicial

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,45 @@
 const botoescarrossel = document.querySelectorAll('.botao');
-const imagens = document.querySelectorAll('.imagem');
+const textos = document.querySelectorAll('.texto');
 
 
 botoescarrossel.forEach((botao, indice) => {
+  botao.addEventListener('click' , () => {
+
+    desativarBotaoSelecionado();
+
+    selecionarBotaoCarrossel1(botao);
+
+    esconderTextoAtivo();
+
+    mostrarTextoDeFundo(indice);
+  }) 
+});
+
+function mostrarTextoDeFundo(indice) {
+  textos[indice].classList.add('ativo');
+}
+
+function selecionarBotaoCarrossel1(botao) {
+  botao.classList.add('selecionado');
+}
+
+function esconderTextoAtivo() {
+  const textoAtivo = document.querySelector('.ativo');
+  textoAtivo.classList.remove('ativo');
+}
+
+function desativarBotaoSelecionado() {
+  const botaoSelecionado = document.querySelector('.selecionado');
+  (botaoSelecionado.classList.remove('selecionado'));
+}
+
+// _________________Carrossel2__________________
+
+const botoescarrossel2 = document.querySelectorAll('.botao2');
+const imagens = document.querySelectorAll('.imagem');
+
+
+botoescarrossel2.forEach((botao2, indice) => {
   botao.addEventListener('click' , () => {
 
     desativarBotaoSelecionado();
